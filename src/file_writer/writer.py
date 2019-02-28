601359192ee9f5a4d8d39641877105150a5714b6
Alessia Mondolo <alessia.mondolo@mail.polimi.it>
2019-02-28 21:55:32 +0100

    Add file writer

---
+++ src/file_writer/writer.py
@@ -0,0 +1,20 @@
+from src.algorithms import Slideshow
+
+class FileWriter(object):
+    def __init__(self, path):
+        with open(path, 'r') as f:
+            self.raw_statement = f.readlines()
+
+    def write(self, filename, slideshow):
+        return self._write(filename, slideshow)
+
+    def _write(self, filename, slideshow):
+        out = open(filename, 'w+')
+        out.write(str(len(slideshow.slides)) + '\n')
+        for slide in slideshow.slides:
+            ids = ''
+            for photo in slide.photos:
+                ids += str(photo.photo_id) + ' '
+            new_line = ids[:-1] + '\n'
+            out.write(new_line)
+        out.close()
